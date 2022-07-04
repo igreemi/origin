80202fab93b12aa863c74de162280c0d6915f5a0
Максим Абрамов <greem@yandex.ru>
2022-07-04 16:14:01 +0300

    Add HW2_Task1

--- HW2/Task_1/HW2_Task1/HW2_Task1/HW2_Task1.cpp
+++ HW2/Task_1/HW2_Task1/HW2_Task1/HW2_Task1.cpp
@@ -12,28 +12,41 @@ enum class MONTH {
     September = 9,
     October = 10,
     November = 11,
-    December = 12
+    December = 12,
+    close = 0
 };
 
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
- //   int s;
 
-   // MONTH s;
+    MONTH month;
+    int num = 0; 
 
-
-
-    MONTH s;
-
-    std::cin >> s;
-
-    if (s == MONTH::January) {
-        std::cout << "На дворе лето!" << std::endl;
-    }
-        
-
-
+    do {
+        std::cout << "Введите номер месяца: ";
+        std::cin >> num;
+        month = static_cast<MONTH>(num);
+        switch (month)
+        {
+        case MONTH::close: std::cout << "До свидания" << std::endl; break;
+        case MONTH::January: std::cout << "Январь" << std::endl; break;
+        case MONTH::February: std::cout << "Февраль" << std::endl; break;
+        case MONTH::March: std::cout << "Март" << std::endl; break;
+        case MONTH::April: std::cout << "Апрель" << std::endl; break;
+        case MONTH::May: std::cout << "Май" << std::endl; break;
+        case MONTH::June: std::cout << "Июнь" << std::endl; break;
+        case MONTH::July: std::cout << "Июль" << std::endl; break;
+        case MONTH::August: std::cout << "Август" << std::endl; break;
+        case MONTH::September: std::cout << "Сентябрь" << std::endl; break;
+        case MONTH::October: std::cout << "Октябрь" << std::endl; break;
+        case MONTH::November: std::cout << "Ноябрь" << std::endl; break;
+        case MONTH::December: std::cout << "Декабрь" << std::endl; break;
+        default: std::cout << "Неправильный номер!" << std::endl; break;
+        }
+    } while (num != 0);
+ 
+    return 0;
 }
 
