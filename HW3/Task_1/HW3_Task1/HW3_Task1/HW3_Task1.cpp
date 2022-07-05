1804107a27c89cb2eaf6bce8ac75e6b9c5cfb790
Максим Абрамов <greem@yandex.ru>
2022-07-05 18:13:50 +0300

    Add HW3_Task1

--- HW3/Task_1/HW3_Task1/HW3_Task1/HW3_Task1.cpp
+++ HW3/Task_1/HW3_Task1/HW3_Task1/HW3_Task1.cpp
@@ -1,44 +1,80 @@
 ﻿#include <iostream>
 
 class Calculator {
-    double num1;
-    double num2;
+public:
+	double num1 = 0;
+	double num2 = 0;
 
-    double add(double num1, double num2) {
-        return num1 + num2;
-    }
-        double multiply(double num1, double num2) {
-            return num1 * num2;
-    }
-        double subtract_1_2(double num1, double num2) {
-            return num2 - num1;
-        }
-        double subtract_2_1(double num1, double num2) {
-            return num1 - num2;
-        }
-        double divide_1_2(double num1, double num2) {
-            return num1 / num2;
-        }
-        double divide_2_1(double num1, double num2) {
-            return num2 / num1;
-        }
-        bool set_num1(double num1) {
-            if (num1 != 0) {
-                this->num1 = num1;
-                return true;
-            }
-            return false;
-        }
-        bool set_num2(double num2) {
-            if (num1 != 0) {
-                this->num2 = num2;
-                return true;
-            }
-            return false;
-        }
+	double add() {
+		return num1 + num2;
+	}
+	double multiply() {
+		return num1 * num2;
+	}
+	double subtract_1_2() {
+		return num1 - num2;
+	}
+	double subtract_2_1() {
+		return num2 - num1;
+	}
+	double divide_1_2() {
+		return num1 / num2;
+	}
+	double divide_2_1() {
+		return num2 / num1;
+	}
+	bool set_num1(double num1) {
+		if (num1 != 0) {
+			this->num1 = num1;
+			return true;
+		}
+		std::cout << "Неверный ввод!" << std::endl;
+		return false;
+	}
+	bool set_num2(double num2) {
+		if (num2 != 0) {
+			this->num2 = num2;
+			return true;
+		}
+		std::cout << "Неверный ввод!" << std::endl;
+		return false;
+	}
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russian");
+
+	Calculator calc;
+
+
+
+	while (true) {
+		double num1 = 0;
+		double num2 = 0;
+		do {
+			std::cout << "Введите num1: ";
+			std::cin >> num1;
+			calc.num1 = num1;
+
+		} while (!calc.set_num1(num1));
+
+
+		do {
+			std::cout << "Введите num2: ";
+			std::cin >> num2;
+			calc.num2 = num2;
+
+		} while (!calc.set_num1(calc.num2));
+
+		std::cout << "num1 + num2 = " << calc.add() << std::endl;
+		std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
+		std::cout << "num2 - num1 = " << calc.subtract_1_2() << std::endl;
+		std::cout << "num1 - num2 = " << calc.subtract_2_1() << std::endl;
+		std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+		std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
+		std::cout << std::endl;
+
+	}
+	return 0;
 }
