b9d7c5026105e4b76c38e01dece94bdac2cd8b63
Максим Абрамов <greem@yandex.ru>
2022-07-05 12:34:57 +0300

    Add HW2_Task3

---
+++ HW2/Task_3/HW2_Task3/HW2_Task3/HW2_Task3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+struct address {
+
+	std::string city; 
+	std::string street;
+	int num_home;
+	int num_appart;
+	int index;
+
+};
+
+void print_address(address p) {
+	std::cout << "Город: " << p.city << std::endl;
+	std::cout << "Улица: " << p.street << std::endl;
+	std::cout << "Номер дома: " << p.num_home << std::endl;
+	std::cout << "Номер квартиры: " << p.num_appart << std::endl;
+	std::cout << "Индекс: " << p.index << std::endl;
+
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	address p = { "Москва", "Арбат", 12, 8, 123456 };
+	print_address(p);
+
+	std::cout << std::endl;
+
+	address dp = { "Ижевск", "Пушкина", 59, 143, 953769 };
+	print_address(dp);
+
+	return 0;
+}
