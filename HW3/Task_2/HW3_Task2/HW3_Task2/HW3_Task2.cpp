7e35fec1b6b50229029bc40210a65ca8558c830f
Максим Абрамов <greem@yandex.ru>
2022-07-07 16:56:58 +0300

    Update HW3_Task2

--- HW3/Task_2/HW3_Task2/HW3_Task2/HW3_Task2.cpp
+++ HW3/Task_2/HW3_Task2/HW3_Task2/HW3_Task2.cpp
@@ -3,7 +3,7 @@
 
 class Counter {
 private:
-<<<<<<< HEAD
+
 	int count = 0;
 	//--------------------------------------
 public:
@@ -27,37 +27,13 @@ public:
 	Counter() {
 
 	}
-=======
-    int count = 0;
-//--------------------------------------
-public:
-    int plus() {
-     return   count++;
-    }
 
-    int minus() {
-        return count--;
-    }
-
-    int stat() {
-        return count;
-    }
- //--------------------------------------
-
-    Counter(int count) {
-        this->count = count;
-    }
-
-    Counter() {
-        count = 1;
-    }
->>>>>>> 1f347790ec73d814c7ca6fdd6c321a2485d5c525
 };
 
 
 int main()
 {
-<<<<<<< HEAD
+
 	setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251);
 	int num = 0;
@@ -107,61 +83,5 @@ int main()
 	} while (true);
 
 	return 0;
-=======
-    setlocale(LC_ALL, "Russian");
-    SetConsoleCP(1251);
-    int num;
-    std::string yes_no;
-    std::string symbol;
 
-    Counter count; //переменная класса Counter
-
-    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
-    std::cin >> yes_no;
-
-    if (yes_no == "да") {
-        
-        std::cout << "Введите начальное значение счётчика: ";
-        std::cin >> num; 
-        count = num;
-        Counter counter(count); // если в конструктор указывать сразу переменную num, то значение count не меняется
-
-    } else
-
-    if (yes_no == "нет") {
-
-        Counter сounter;
-
-    }
-    else
-
-    if (yes_no != "да" && yes_no != "нет") {
-
-        std::cout << "Не верный ввод" << std::endl;
-        return 1;
-
-    }
-
-
-    do {
-        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
-        std::cin >> symbol;
-
-        if (symbol == "+") {
-            count.plus();
-        }
-        if (symbol == "-") {
-            count.minus();
-        }
-        if (symbol == "=") {
-            std::cout << count.stat() << std::endl;
-        }
-        if (symbol == "x" || symbol == "х") {
-            std::cout << "До свидания!" << std::endl;
-            return false;
-        }
-    } while (true);
-
-    return 0;
->>>>>>> 1f347790ec73d814c7ca6fdd6c321a2485d5c525
 }
