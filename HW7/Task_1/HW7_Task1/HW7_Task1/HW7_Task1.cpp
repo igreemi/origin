ee5886b2fa325cfc04526cc5e5f4457b28d8a01d
Максим Абрамов <greem@yandex.ru>
2022-07-21 13:13:45 +0300

    Add HW7

---
+++ HW7/Task_1/HW7_Task1/HW7_Task1/HW7_Task1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+#define MODE 1
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+
+#if MODE == 0
+
+	std::cout << "Работаю в режиме тренировки.";
+
+#elif MODE == 1
+
+	std::cout << "Работаю в боевом режиме" << std::endl;
+
+	int a, b;
+
+	std::cout << "Введите число 1: ";
+	std::cin >> a;
+
+	std::cout << "Введите число 1: ";
+	std::cin >> b;
+
+#include "add_func.h"
+
+	std::cout << "Результат сложения: " << add(a, b) << std::endl; 
+#else 
+
+	std::cout << "Не известный режим. Завершаем работу.";
+	return 1;
+#endif
+
+	return 0;
+}
---
+++ HW7/Task_2/HW7_Task2/HW7_Task2/HW7_Task2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <windows.h>
+
+#define SUB(a, b) ((a) - (b))
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+
+	int a = 6;
+	int b = 5;
+	int c = 2;
+
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+
+}
