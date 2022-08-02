0c390082b5e380bd2e1085979e65695574fef176
Максим Абрамов <greem@yandex.ru>
2022-08-02 12:55:36 +0300

    Fix HW8

--- HW8/Task_2/HW8_Task2/HW8_Task2/Quadrilateral.cpp
+++ HW8/Task_2/HW8_Task2/HW8_Task2/Quadrilateral.cpp
@@ -12,7 +12,7 @@ Quadrilateral::Quadrilateral(int a, int b, int c, int d, int A, int B, int C, in
 	figure = "Quadrilateral";
 	side_num = side;
 
-	if (side_num != 4 || (A)+(B)+(C)+(D) != 360) {
+	if (side_num != 4 || (A + B + C + D) != 360) {
 		throw FigureEx(figure + "(sides " + std::to_string(a) + ", "
 			+ std::to_string(b) + ", " + std::to_string(c) + ", " + std::to_string(d) + "; angles "
 			+ std::to_string(A) + ", " + std::to_string(B) + ", " + std::to_string(C) + ", " + std::to_string(D) + ")");
