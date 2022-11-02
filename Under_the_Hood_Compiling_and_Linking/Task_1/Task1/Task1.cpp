5cc7cbffb259f9de00c11adeacbc1fe42cbb8175
Abramov Maxim <greem@yandex.ru>
2022-11-02 11:35:55 +0300

    Add Under_the_Hood_Compiling_and_Linking

---
+++ Under_the_Hood_Compiling_and_Linking/Task_1/Task1/Task1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+struct point {
+	double m_x;
+	double m_y;
+	point(double x, double y) {
+		m_x = x;
+		m_y = y;
+	}
+};
+
+void print_point(const point& point_object) {
+	std::cout << "x: " << point_object.m_x << ", y: "
+		<< point_object.m_y << std::endl;
+}
+
+int main()
+{
+	int i;
+	for (i = 0; i < 5; i++){
+		double a = i;
+		point my_point(a, 2 * a);
+		print_point(my_point);
+	}
+	return 0;
+}
---
+++ Under_the_Hood_Compiling_and_Linking/Task_2/Task2/Task2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+//#include "linking_error.h"//указан не существующий фаил
+
+
+void syntax_error(int a, int b)
+{
+	for (i = 0; i < b; i++)
+	{
+		std::cout << c = a + b << ;
+	}
+}// обьявить переменную i и c, в строке вывода убрать оператор << или дописать std::endl 
+
+
+void semantic_error(int a, int b)
+{
+	for (unsigned int i = 10; i >= 0; --i)
+	{
+
+		std::cout << a + b << std::endl;
+	}
+}//заменить оператор >= на >, в противном случае цикл не завершится
+
+class linking_error {
+public:
+	linking_error() {};
+	int sum(int a, int b) {
+		return a + b;
+	}
+}; //не определен конструктор
+
+int main()
+{
+	std::cout << "Hello World!\n";
+
+	semantic_error(3, 6);
+	linking_error a;
+
+}
