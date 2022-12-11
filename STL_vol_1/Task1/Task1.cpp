d8f5982b4f0c9fdcdfec50a7f3d1a7635da494ef
Abramov Maxim <greem@yandex.ru>
2022-12-11 23:15:17 +0300

    Add STL_vol_1/

---
+++ STL_vol_1/Task1/Task1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <map>
+#include <vector>
+
+
+int main()
+{
+
+	std::vector<char> my_vec = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '!' };
+
+	std::cout << "[IN] : ";
+
+	for (auto& elem : my_vec)
+	{
+		std::cout << elem;
+	}
+
+	std::cout << std::endl;
+
+	std::map<char, int> counter;
+	for (auto& elem : my_vec)
+	{
+		counter[elem]++;
+	}
+
+	std::multimap<int, char> reverseCount;
+	for (auto& elem : counter)
+	{
+		reverseCount.emplace(elem.second, elem.first);
+	}
+
+	std::multimap<int, char>::reverse_iterator it = reverseCount.rbegin();
+
+	std::cout << "[OUT] : " << std::endl;
+
+	while (it != reverseCount.rend())
+	{
+		std::cout << it->first << ": " << it->second << std::endl;
+		++it;
+	}
+
+	return 0;
+}
---
+++ STL_vol_1/Task2/Task2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+
+int main()
+{
+	std::cout << "[IN]: " << std::endl;
+	int s;
+	std::cin >> s;
+	std::cout << std::endl;
+	std::vector<int> arr(s);
+
+	for (auto& elem : arr)
+	{
+		std::cin >> elem;
+	}
+
+	std::set<int> arrOut;
+
+	for (auto& elem : arr)
+	{
+		arrOut.insert(elem);
+	}
+
+	std::set <int>::reverse_iterator it;
+
+	std::cout << "[OUT]:" << std::endl;
+
+	for (it = arrOut.rbegin(); it != arrOut.rend(); it++)
+	{
+		std::cout << *it << std::endl;
+	}
+
+	return 0;
+
+}
