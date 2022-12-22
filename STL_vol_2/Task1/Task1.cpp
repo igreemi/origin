55a0abe1e95d95ce43a3d357eebd5e9b4d21a977
Abramov Maxim <greem@yandex.ru>
2022-12-22 11:04:10 +0300

    Add STL_vol_2/

---
+++ STL_vol_2/Task1/Task1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+
+std::vector<T> unique_vector(std::vector<T>& v)
+{
+
+	std::sort(v.begin(), v.end());
+
+	auto last = std::unique(v.begin(), v.end());
+
+	v.erase(last, v.end());
+
+	return v;
+
+}
+
+
+int main()
+{
+
+	std::vector<int> v = { 1, 1, 2, 5, 6, 1, 2, 4 };
+
+	std::cout << "IN: ";
+
+	for (auto& i : v)
+	{
+		std::cout << i << " ";
+	}
+
+	std::cout << std::endl;
+
+	unique_vector(v);
+
+	std::cout << "OUT: ";
+
+	for (auto& i : v)
+	{
+		std::cout << i << " ";
+	}
+
+}
+/*
+[IN] : 1 1 2 5 6 1 2 4
+[OUT] : 1 2 4 5 6
+*/
---
+++ STL_vol_2/Task2/Task2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+#include <list>
+#include <vector>
+
+template <class T>
+
+void print_container(const T& container)
+{
+
+	for (const auto& el : container)
+	{
+		std::cout << el << " ";
+	}
+
+	std::cout << std::endl;
+
+}
+
+int main()
+{
+
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+
+	print_container(test_set);
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+
+	print_container(test_list);
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+
+	print_container(test_vector);
+
+	return 0;
+
+}
