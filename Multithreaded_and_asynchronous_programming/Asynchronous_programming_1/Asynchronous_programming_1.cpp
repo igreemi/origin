aa73981cc52aa004d1ce6eb7f8960062789733d9
Abramov Maxim <greem@yandex.ru>
2023-08-08 15:26:58 +0300

    Add Asynchronous_programming

---
+++ Multithreaded_and_asynchronous_programming/Asynchronous_programming_1/Asynchronous_programming_1.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <future>
+#include <vector>
+#include <thread>
+
+int min_element(const std::vector<int> & vec, int i)
+{
+	int min_i = i;
+
+	for (int j = i + 1; j < vec.size(); j++)
+	{
+		if (vec[j] < vec[min_i])
+		{
+			min_i = j;
+		}
+	}
+	return min_i;
+}
+
+void selectionSort(std::vector<int>& vec)
+{
+	for (int i = 0; i < vec.size() - 1; i++)
+	{
+		std::promise<int> minProm;
+
+		std::future<int> minFt = minProm.get_future();
+
+		std::thread t([&vec, i, &minProm]() 
+			{
+		minProm.set_value(min_element(vec, i));
+			});
+
+		int min = minFt.get();
+
+		t.join();
+
+		std::swap(vec[i], vec[min]);
+	}
+}
+
+int main()
+{
+	std::vector<int> vec = { 93, 5, 25, 7, 12, 45, 22, 90 };
+
+	std::cout << "Source Vector: ";
+
+	for (int i : vec) {
+		std::cout << i << " ";
+	}
+
+	std::cout << std::endl;
+
+	selectionSort(vec);
+
+	std::cout << "Sorted vector: ";
+
+	for (int i : vec) {
+		std::cout << i << " ";
+	}
+
+	std::cout << std::endl;
+
+	return 0;
+}
---
+++ Multithreaded_and_asynchronous_programming/Asynchronous_programming_2/Asynchronous_programming_2.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <future>
+
+
+template<typename It, typename Func>
+void parallel_for_each(It begin, It end, Func function)
+{
+	auto curr_size = std::distance(begin, end);
+
+	if (curr_size <= 25)
+	{
+		std::for_each(begin, end, function);
+	}
+	else
+	{
+		auto mid = begin;
+
+		std::advance(mid, curr_size / 2);
+
+		auto ft = std::async(std::launch::async, parallel_for_each<It, Func>, begin, mid, function);
+		auto ft2 = std::async(std::launch::async, parallel_for_each<It, Func>, mid, end, function);
+
+		ft.get();
+		ft2.get();
+	}
+}
+
+void sum(int& i)
+{
+	i += i;
+
+	std::cout << "id " << std::this_thread::get_id() << " | " << i << " | ";
+}
+
+int main()
+{
+	std::vector<int> vec(50, 2);
+
+	for (auto i : vec)
+	{
+		std::cout << i << " ";
+	}
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	parallel_for_each(vec.begin(), vec.end(), sum);
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	for (auto i : vec)
+	{
+		std::cout << i << " ";
+	}
+
+	std::cout << std::endl;
+
+	return 0;
+}
